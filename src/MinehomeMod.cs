e316f1bcc392b5150385a65b4cdf8060ec6b78ee
Lykam <lycam.twitch@gmail.com>
2026-04-29 19:02:45 -0400

    created basic mod setup

---
+++ src/MinehomeMod.cs
@@ -0,0 +1,28 @@
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+
+namespace Mineholme;
+
+public class MinehomeMod : ModSystem
+{
+    public override void Start(ICoreAPI api)
+    {
+        base.Start(api);
+    }
+
+    public override void StartServerSide(ICoreServerAPI api)
+    {
+        base.StartServerSide(api);
+    }
+
+    public override void StartClientSide(ICoreClientAPI api)
+    {
+        base.StartClientSide(api);
+    }
+
+    public override void Dispose()
+    {
+        base.Dispose();
+    }
+}
